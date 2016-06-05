b52c64b32f221106249b26945b29a2ca526a153c
Drew Miley <dmiley@scottlogic.co.uk>
2016-06-05 21:43:36 +0100

    Filtering in progress

--- scripts/viewModel.js
+++ scripts/viewModel.js
@@ -44,6 +44,82 @@ define(['ko', 'util', 'calculator', 'data/sort'],
 		});
 	}
 
+	// function FilterModel(filters, records) {
+	// 	var self = this;
+	// 	self.records = GetObservableArray(records);
+	// 	self.filters = ko.observableArray(filters);
+	// 	self.activeFilters = ko.computed(function() {
+	// 		var filters = self.filters();
+	// 		var activeFilters = [];
+	// 		for (var index = 0; index < filters.length; index++) {
+	// 			var filter = filters[index];
+	// 			if (filter.CurrentOption) {
+	// 				var filterOption = filter.CurrentOption();
+	// 				if (filterOption && filterOption.FilterValue != null) {
+	// 					var activeFilter = {
+	// 						Filter: filter,
+	// 						IsFiltered: function(filter, record) {
+	// 							var filterOption = filter.CurrentOption();
+	// 							if (!filterOption) {
+	// 								return;
+	// 							}
+
+	// 							var recordValue = filter.RecordValue(record);
+	// 							return recordValue != filterOption.FilterValue;NoMat
+	// 						}
+	// 					};
+	// 					activeFilters.push(activeFilter);
+	// 				}
+	// 			} else if (filter.Value) {
+	// 				var filterValue = filter.Value();
+	// 				if (filterValue && filterValue != "") {
+	// 					var activeFilter = {
+	// 						Filter: filter,
+	// 						IsFiltered: function(filter, record) {
+	// 							var filterValue = filter.Value();
+	// 							filterValue = filterValue.toUpperCase();
+								
+	// 							var recordValue = filter.RecordValue(record);
+	// 							recordValue = recordValue.toUpperCase();
+	// 							return recordValue.indexOf(filterValue) == -1;
+	// 						}
+	// 					};
+	// 					activeFilters.push(activeFilter);
+	// 				}
+	// 			}
+	// 		}
+
+	// 		return activeFilters;
+	// 	});
+	// 	self.filteredRecords = ko.computed(function() {
+	// 		var records = self.records();
+	// 		var filters = self.activeFilters();
+	// 		if (filters.length == 0) {
+	// 			return records;
+	// 		}
+			
+	// 		var filteredRecords = [];
+	// 		for (var rIndex = 0; rIndex < records.length; rIndex++) {
+	// 			var isIncluded = true;
+	// 			var record = records[rIndex];
+	// 			for (var fIndex = 0; fIndex < filters.length; fIndex++) {
+	// 				var filter = filters[fIndex];
+	// 				var isFiltered = filter.IsFiltered(filter.Filter, record);
+	// 				if (isFiltered) {
+	// 					isIncluded = false;
+	// 					break;
+	// 				}
+	// 			}
+				
+	// 			if (isIncluded) {
+	// 				filteredRecords.push(record);
+	// 			}
+	// 		}
+
+	// 		return filteredRecords;
+	// 	});
+	// }
+
 	var ViewModel = function() {
 	    var self = this;
 
@@ -56,6 +132,27 @@ define(['ko', 'util', 'calculator', 'data/sort'],
 
 		var leagueFixtures = calculator.formLeagueFixtures();
 		self.leagueFixtures = ko.observable(leagueFixtures);
+
+		// var filters = [{
+		// 	Type: "text",
+		// 	Name: "Name",
+		// 	Value: ko.observable(""),
+		// 	RecordValue: function(record) { return record.name; }
+		// },
+		// {
+		// 	Type: "select",
+		// 	Name: "Status",
+		// 	Options: [
+		// 		GetOption("All", "All", null),
+		// 		GetOption("None", "None", "None"),
+		// 		GetOption("New", "New", "New"),
+		// 		GetOption("Recently Modified", "Recently Modified", "Recently Modified")
+		// 	],
+		// 	CurrentOption: ko.observable(),
+		// 	RecordValue: function(record) { return record.status; }
+		// }];
+
+		// self.filter = new FilterModel(filters, self.leagueFixtures);
 	}
      
     return ViewModel;
