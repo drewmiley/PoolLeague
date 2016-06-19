f2bdd6772cdea7b9fa36feac20c4b292f290ce3f
Drew Miley <dmiley@scottlogic.co.uk>
2016-06-19 13:46:16 +0100

    Fixture grid works

--- scripts/calculator/fixtureGridFormatter.js
+++ scripts/calculator/fixtureGridFormatter.js
@@ -1,17 +1,30 @@
 define(['data/matches', 'data/fixtures'],
 	function(matches, fixtures) {
 
-	function LeagueFixture(gameWeek, homePlayer, homeScore, awayPlayer, awayScore) {
-		var self = this;
-		self.gameWeek = gameWeek;
-		self.homePlayer = homePlayer;
-		self.homeScore = homeScore;
-		self.awayPlayer = awayPlayer;
-		self.awayScore = awayScore;
-	}
+	return function fixtureGridFormatter(firstPlayerID, secondPlayerID) {
+		if (firstPlayerID === secondPlayerID) {
+			return 'X';
+		}
 
-	return function fixtureGridFormatter(firstPlayerId, secondPlayerId) {
-		return firstPlayerId * secondPlayerId;
+		var homeFixture = fixtures.filter(function(fixture) {
+			return fixture.homePlayerID === firstPlayerID && fixture.awayPlayerID === secondPlayerID;
+		});
+
+		var awayFixture = fixtures.filter(function(fixture) {
+			return fixture.homePlayerID === secondPlayerID && fixture.awayPlayerID === firstPlayerID;
+		});
+
+		var formattedFixture = '';
+
+		if (homeFixture.length === 1) {
+			var match = matches.filter(function(match) { return match.fixtureID === homeFixture[0].id });
+			return match.length === 0 ? homeFixture[0].gameWeek : match[0].homeScore + ' - ' + match[0].awayScore;
+		} else if (awayFixture.length === 1) {
+			var match = matches.filter(function(match) { return match.fixtureID === awayFixture[0].id });
+			return match.length === 0 ? awayFixture[0].gameWeek : match[0].awayScore + ' - ' + match[0].homeScore;
+		}
+
+		return formattedFixture;
 	}
 
 })
