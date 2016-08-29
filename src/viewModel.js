537f958ae656f2ce281d8634fdcc41963b23c2ae
Drew Miley <dmiley@scottlogic.co.uk>
2016-08-29 09:59:28 +0100

    Refactor of viewModel

--- src/viewModel.js
+++ src/viewModel.js
@@ -1,82 +1,62 @@
 import calculator from './logic/calculator';
 import modify from './logic/modify';
 
-var util = modify.util;
-
 function Sorter(records) {
-	var self = this;
+	let self = this;
 	self.records = records;
 
-	var leagueSort = modify.leagueSort;
+	self.directions = modify.leagueSort.directions;
+	self.options = modify.leagueSort.options;
 
-	self.options = leagueSort.options;
-	self.directions = leagueSort.directions;
-
-	self.currentOption = self.options[0];
 	self.currentDirection = self.directions[0];
+	self.currentOption = self.options[0];
 
-	var previousDirection = leagueSort.directions.filter(function(direction) { return direction.isDefault; })[0].sort;
-	var previousOption = leagueSort.options.filter(function(option) { return option.isDefault; })[0].sort;
+	let previousDirection = self.directions.filter((direction) => { return direction.isDefault; })[0].sort;
+	let previousOption = self.options.filter((option) => { return option.isDefault; })[0].sort;
 
-	self.ordered = function() {
-		var records = self.records;
-		var sortOption = self.currentOption;
-		var sortDirection = self.currentDirection;
-
-		if (sortOption == null || sortDirection == null) {
-			return records;
+	self.ordered = () => {
+		if (self.currentOption == null || self.currentDirection == null) {
+			return self.records;
 		}
 
-		var sortedArray = util.SortArray(records,
-			sortDirection.sort,
-			sortOption.sort,
+		const sortedArray = modify.util.SortArray(self.records,
+			self.currentDirection.sort,
+			self.currentOption.sort,
 			previousDirection,
 			previousOption);
 
-		previousDirection = sortDirection.sort;
-		previousOption = sortOption.sort;
+		previousDirection = self.currentDirection.sort;
+		previousOption = self.currentOption.sort;
 
 		return sortedArray;
 	};
 }
 
 function Filter(records) {
-	var self = this;
+	let self = this;
 	self.records = records;
 
-	var fixtureFilter = modify.fixtureFilter;
+	self.filters = modify.fixtureFilter.options;
 
-	self.filters = fixtureFilter.options;
-
-	self.activeFilters = function() {
-		var filters = self.filters;
-		var activeFilters = [];
-
-		return filters.filter(function(filter) { return (filter.Type === 'select' && filter.CurrentOption.Value != null) ||
+	self.activeFilters = () => {
+		return self.filters.filter((filter) => { return (filter.Type === 'select' && filter.CurrentOption.Value != null) ||
 				(filter.Type === 'text' && filter.CurrentText);});
 	};
 
-	self.filtered = function() {
-		var records = self.records;
-		var activeFilters = self.activeFilters();
-
-		return records.filter(function(record) { return !util.IsFiltered(record, activeFilters); });
+	self.filtered = () => {
+		return self.records.filter((record) => { return !modify.util.IsFiltered(record, self.activeFilters()); });
 	};
 }
 
 export default function ViewModel() {
-    var self = this;
+    let self = this;
 
     self.gameWeek = calculator.gameWeek;
 
-    var leagueTable = calculator.leagueTable();
-    self.leagueTable = leagueTable;
-
+    self.leagueTable = calculator.leagueTable();
 	self.leagueTableSorter = new Sorter(self.leagueTable);
 
-	var leagueFixtures = calculator.leagueFixtures();
-	self.leagueFixtures = leagueFixtures;
-
+	self.leagueFixtures = calculator.leagueFixtures();
 	self.leagueFixturesFilter = new Filter(self.leagueFixtures);
 
 	self.players = calculator.players;
