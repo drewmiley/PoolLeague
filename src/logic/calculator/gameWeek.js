f2a4ffe56c417482eba930708bec519b4357e37d
Drew Miley <dmiley@scottlogic.co.uk>
2016-09-01 09:55:25 +0100

    Gameweek generated from date

--- src/logic/calculator/gameWeek.js
+++ src/logic/calculator/gameWeek.js
@@ -1,3 +1,13 @@
-import gameWeek from '../data/gameWeek';
+import gameWeekDates from '../data/gameWeekDates';
+
+const now = new Date();
+
+const gameWeek = (() => {
+    let i = 0;
+    while (now > gameWeekDates[i]) {
+        i++;
+    }
+    return i;
+})();
 
 export default gameWeek;
--- src/logic/data/gameWeek.js
+++
@@ -1,3 +0,0 @@
-const gameWeek = 3;
-
-export default gameWeek;
