292fd92f8962211a30c7209b19f08dcd82ad6622
Drew Miley <dmiley@scottlogic.co.uk>
2016-09-01 16:39:27 +0100

    Makes reducer act on immutable state

--- src/reducers/poolLeague.js
+++ src/reducers/poolLeague.js
@@ -1,72 +1,82 @@
 import * as actions from '../constants/actions';
 
-function setState(state, newState) {
-    if (!newState) {
-        return state;
+function setState(state, updatedState) {
+    let newState = state;
+    if (!updatedState) {
+        return newState;
     }
-    state = newState;
-    return state;
+    newState = updatedState;
+    return newState;
 }
 
 function setDisplayedDiv(state, displayClass) {
+    let newState = state;
     if (!displayClass) {
-        return state;
+        return newState;
     }
-    state.displayedClass = displayClass;
-    return state;
+    newState.displayedClass = displayClass;
+    return newState;
 }
 
 function setLeagueTableSortDirection(state, direction) {
+    let newState = state;
     if (!direction) {
-        return state;
+        return newState;
     }
-    state.leagueTableSorter.currentDirection = state.leagueTableSorter.directions.filter((item) => {
+    newState.leagueTableSorter.currentDirection = newState.leagueTableSorter.directions.filter((item) => {
         return item.value === direction;
     })[0];
-    return state;
+    return newState;
 }
 
 function setLeagueTableSortOption(state, option) {
+    let newState = state;
     if (!option) {
-        return state;
+        return newState;
     }
-    state.leagueTableSorter.currentOption = state.leagueTableSorter.options.filter((item) => {
+    newState.leagueTableSorter.currentOption = newState.leagueTableSorter.options.filter((item) => {
         return item.value === option;
     })[0];
-    return state;
+    return newState;
 }
 
 function clearLeagueSort(state) {
-    state.leagueTableSorter.currentDirection = state.leagueTableSorter.directions.filter((direction) => { return direction.isDefault; })[0];
-    state.leagueTableSorter.currentOption = state.leagueTableSorter.options.filter((option) => { return option.isPrimary; })[0];
+    let newState = state;
 
-    state.leagueTableSorter.previousDirection = state.leagueTableSorter.directions.filter((direction) => { return direction.isDefault; })[0];
-    state.leagueTableSorter.previousOption = state.leagueTableSorter.options.filter((option) => { return option.isSecondary; })[0];
+    newState.leagueTableSorter.currentDirection = newState.leagueTableSorter.directions.filter((direction) => { return direction.isDefault; })[0];
+    newState.leagueTableSorter.currentOption = newState.leagueTableSorter.options.filter((option) => { return option.isPrimary; })[0];
 
-    return state;
+    newState.leagueTableSorter.previousDirection = newState.leagueTableSorter.directions.filter((direction) => { return direction.isDefault; })[0];
+    newState.leagueTableSorter.previousOption = newState.leagueTableSorter.options.filter((option) => { return option.isSecondary; })[0];
+
+    return newState;
 }
 
 function setFixtureFilter(state, value, filter) {
+    let newState = state;
+
     if (!filter || value === null || value === undefined) {
-        return state;
+        return newState;
     }
     if (filter.type === 'select') {
-        const selectFilter = state.leagueFixturesFilter.filters.filter((item) => {
+        const selectFilter = newState.leagueFixturesFilter.filters.filter((item) => {
             return item == filter;
         })[0];
         selectFilter.currentOption = selectFilter.options.filter((item) => {
             return item.name == value;
         })[0];
     } else if (filter.type === 'text') {
-        state.leagueFixturesFilter.filters.filter((item) => {
+        newState.leagueFixturesFilter.filters.filter((item) => {
             return item == filter;
         })[0].currentText = value;
     }
-    return state;
+    return newState;
 }
 
 function clearFixtureFilter(state) {
-    state.leagueFixturesFilter.filters.forEach((filter) => {
+    let newState = state;
+    
+    newState.leagueFixturesFilter.filters.forEach((filter) => {
         if (filter.type === 'select') {
             filter.currentOption = filter.options.filter((option) => { return option.isDefault; })[0];
         } else if (filter.type === 'text') {
@@ -74,7 +84,7 @@ function clearFixtureFilter(state) {
         }
     });
 
-    return state;
+    return newState;
 }
 
 export default function(state = {}, action) {
