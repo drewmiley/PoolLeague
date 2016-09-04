85e3c6c2dda4ba0553b088df8b74dac4b2189b03
Drew Miley <dmiley@scottlogic.co.uk>
2016-09-04 23:15:20 +0100

    Deploy to gh-pages

--- src/data/gameWeekDates.js
+++
@@ -1,9 +0,0 @@
-const gameWeekDates = [
-    new Date('2016-08-17'),
-    new Date('2016-08-24'),
-    new Date('2016-08-31'),
-    new Date('2016-09-07'),
-    new Date('2016-09-14')
-];
-
-export default gameWeekDates;
